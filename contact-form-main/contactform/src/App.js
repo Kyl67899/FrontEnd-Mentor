e2802f84151b9137a9714b4429289f9dd4182ec8
Kyle Y. Parsotan <35744262+Kyl67899@users.noreply.github.com>
2024-06-10 21:53:50 -0400

    Added project

--- contact-form-main/contactform/src/App.js
+++ contact-form-main/contactform/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 
 function App() {
+
+  function submit(){
+    // alert('submit')
+  }
+
   return (
     <>
       <main className="">
         <div className="container">
           <div className="box">
             <div className="title">
-              <h1 className="">
+              <h1 className="title-name">
                 Contact Us
               </h1>
             </div>
@@ -15,7 +20,7 @@ function App() {
               <div className="name-col">
 
                 <div className="firstname">
-                  <div className="firstName">
+                  <div className="form-title">
                     First Name <span className="star">*</span>
                   </div>
                   <input 
@@ -25,11 +30,13 @@ function App() {
                     required 
                   />
                   {/* error */}
-                  {/* This field is required */}
+                  <div className="error">
+                    This field is required
+                  </div>
                 </div>
 
                 <div className="firstname">
-                  <div className="firstName">
+                  <div className="form-title">
                     Last Name <span className="star">*</span>
                   </div>
                   <input 
@@ -39,13 +46,15 @@ function App() {
                     required 
                   />
                   {/* error */}
-                  {/* This field is required */}
+                  <div className="error">
+                    This field is required
+                  </div>
                 </div>
 
               </div>
               
               <div className="emailForm">
-                <div className="Email">
+                <div className="form-title">
                   Email Address <span className="star">*</span>
                 </div>
                 <input 
@@ -56,44 +65,70 @@ function App() {
                   required 
                 />
                 {/* error */}
-                {/* This field is required */}
+                {/* error */}
+                <div className="error">
+                  Please enter a valid email address
+                </div>
+                
+              </div>
+              
+              <div className="form-title">
+                Query Type <span className="star">*</span>
+              </div>
+              <div className="wrapper">
+                <label for="option-1" className="option option-1">
+                  <input 
+                    type="radio" 
+                    name="select" 
+                    id="option-1" 
+                    className="dot" 
+                  />
+                  {/* <div className="dot"></div> */}
+                  <span>General Enquiry</span>
+                </label>
+
+                <label for="option-2" className="option option-2">
+                  <input 
+                    type="radio" 
+                    name="select" 
+                    id="option-2" 
+                    className="dot" 
+                  />
+                  {/* <div className="dot"></div> */}
+                  <span>Support Request</span>
+                </label>
               </div>
 
-              <div className="name-col">
+              <div className="wrapper-1">
+                <div className="form-title form-title-1">
+                  Message <span>*</span>
+                </div>
+                <textarea
+                  rows={4}
+                  cols={70}
+                >
 
-                <div className="queryType">
-                  <div className="queryType-1">
-                    <h4 className="queryType-name">
-                      Query Type <span className="star">*</span>
-                    </h4>
-                    <div className="queryType-btn">
-                      <input 
-                        type="radio" 
-                        name="btn" 
-                        className="btn" 
-                        required 
-                      />
-                      <span className="queryType-btn--title">
-                        General Enquiry
-                      </span>
-                    </div>
-                    <div className="queryType-btn">
-                      <input 
-                        type="radio" 
-                        name="btn" 
-                        className="btn" 
-                        required 
-                      />
-                      <span className="queryType-btn--title">
-                        Support Request
-                      </span>
-                    </div>
+                </textarea>
+
+              </div>
+
+              <div className="bottom">
+                <div className="bottom-content">
+                  <input 
+                    type="checkbox" 
+                  />
+                  <div className="form-title consent">
+                    I consent to being contacted by the team <span>*</span>
                   </div>
-                  {/* error */}
-                  {/* Please select a query type */}
                 </div>
 
               </div>
+
+              <input 
+                type="submit" 
+                className="btn"
+                onClick={submit()}
+              />
 
             </div>
 
